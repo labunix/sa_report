652267224d4cea2d52161bbec5b0716ebd5ca3c7
labunix <labunix@gmail.com>
2019-10-14 23:45:38 +0900

    update sa_report.sh

--- sa_report.sh
+++ sa_report.sh
@@ -1,53 +1,17 @@
 #!/bin/bash
+# dpkg -l sysstat
+#
+# IDLE=8
+# KBMEMFREE=2
+# KBMEMUSED=3
+#
+# Yesterday,Average CPU(idle all),MEM(kbmemfree),MEM(kbmemused)
+# example output
+# 2019/10/13,76.43,545149,2325499
 
-LOGDIR=/var/log/sysstat
-YMDATE=`env LANG=C date --date "1 days ago" '+%Y%m'`
-TARGET=`env LANG=C date --date "1 days ago" '+%d'`
-YESTERDAY="${LOGDIR}/sa${TARGET}"
-CPULOG="`hostname -s`_${YMDATE}${TARGET}_cpuidle.log"
-MEMFREELOG="`hostname -s`_${YMDATE}${TARGET}_kbmemfree.log"
-MEMUSEDLOG="`hostname -s`_${YMDATE}${TARGET}_kbmemused.log"
+dpkg -l sysstat | grep ^ii >/dev/null && \
+  echo -e "-u 8\n-r 2\n-r 3" | \
+  awk -v yesterday=$(date -d "yesterday" '+%d') \
+    '{print "echo -n $(env LANG=C sar",$1,"-f /var/log/sysstat/sa"yesterday" | awk \047/Average/{print $"$2"}\047);echo -n ,"}' | \
+  sh | awk -v yesterday=$(date -d "yesterday" '+%Y/%m/%d') '{gsub(",$","",$0);print yesterday","$0}'
 
-IDLE=8
-env LANG=C sar -u -f "${YESTERDAY}" | awk '{print $'${IDLE}'}' | \
-  grep idle > /dev/null 2>&1 || exit ${IDLE}
-
-KBMEMFREE=2
-env LANG=C sar -r -f "${YESTERDAY}" | awk '{print $'$KBMEMFREE'}' | \
-  grep kbmemfree > /dev/null 2>&1 || exit ${KBMEMFREE}
-
-KBMEMUSED=3
-env LANG=C sar -r -f "${YESTERDAY}" | awk '{print $'$KBMEMUSED'}' | \
-  grep kbmemused > /dev/null 2>&1 || exit ${KBMEMUSED}
-
-
-if [ "x${IDLE}" == "x" ];then
-  echo "ERROR: Not Found %idle"
-  exit 1
-fi
-if [ "x${KBMEMFREE}" == "x" ];then
-  echo "ERROR: Not Found kbmemfree"
-  exit 1
-fi
-if [ "x${KBMEMUSED}" == "x" ];then
-  echo "ERROR: Not Found kbmemused"
-  exit 1
-fi
-  
-env LANG=C sar -u -f "${YESTERDAY}" | grep "^[0-9].*all" | \
-  awk '{print $1","$'${IDLE}'}' > ${CPULOG}
-env LANG=C sar -r -f "${YESTERDAY}" | grep -v "[A-z]\|^\$" | \
-  awk '{print $1","$'${KBMEMFREE}'}' > ${MEMFREELOG}
-env LANG=C sar -r -f "${YESTERDAY}" | grep -v "[A-z]\|^\$" | \
-  awk '{print $1","$'${KBMEMUSED}'}' > ${MEMUSEDLOG}
-unset LOGDIR
-unset YMDATE
-unset TARGET
-unset YESTERDAY
-unset CPULOG
-unset MEMFREELOG
-unset MEMUSEDLOG
-unset IDLE
-unset KBMEMUSED
-unset KBMEMFREE
-exit 0
